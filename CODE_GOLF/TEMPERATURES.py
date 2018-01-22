06cd63fc1f930c09ef0a3f1e79256dc657e01662
Python-script-writer <33414816+Python-script-writer@users.noreply.github.com>
2018-01-22 22:06:07 +0200

    Update TEMPERATURES.py

--- CODE_GOLF/TEMPERATURES.py
+++ CODE_GOLF/TEMPERATURES.py
@@ -1,1 +1,1 @@
-print(sorted( map(int, input().split()), key = lambda x: (x**2,-x))[0] if int(input()) else 0)
+print(sorted(map(int, input().split()), key = lambda x: (x**2,-x))[0] if int(input()) else 0)
